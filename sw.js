214085a463c9147ce6e29182f310b0fdc6ee1210
diogocanto <diogoribeirocanto@gmail.com>
2026-07-26 21:12:52 -0300

    feat: add comprehensive multi-resolution icon set and update manifest, service worker, and HTML references

--- sw.js
+++ sw.js
@@ -1,12 +1,27 @@
 // NovoControle Service Worker - PWA & Cross-Platform Offline Support
 // Compatível com Safari/WebKit iOS 12+, Chrome, Firefox, Edge
-const CACHE_NAME = 'novocontrole-v7';
+const CACHE_NAME = 'novocontrole-v8';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './styles.css',
   './icon.png',
   './manifest.json',
+  './icons/icon-16x16.png',
+  './icons/icon-32x32.png',
+  './icons/icon-48x48.png',
+  './icons/icon-72x72.png',
+  './icons/icon-96x96.png',
+  './icons/icon-120x120.png',
+  './icons/icon-128x128.png',
+  './icons/icon-144x144.png',
+  './icons/icon-152x152.png',
+  './icons/icon-167x167.png',
+  './icons/icon-180x180.png',
+  './icons/icon-192x192.png',
+  './icons/icon-256x256.png',
+  './icons/icon-384x384.png',
+  './icons/icon-512x512.png',
   './js/app.js',
   './js/supabaseClient.js',
   './js/charts.js',
